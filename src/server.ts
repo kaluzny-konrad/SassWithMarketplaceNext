b3a77c5cc7de3132ef28a341e33f7039079c458e
Konrad Kałużny <konrad.kaluzny@hotmail.com>
2024-01-11 18:34:15 +0100

    delete unused code

--- src/server.ts
+++ src/server.ts
@@ -3,11 +3,9 @@ import { getPayloadClient } from "./get-payload";
 import { nextApp, nextHandler } from "./next-utils";
 import * as trpcExpress from "@trpc/server/adapters/express";
 import { appRouter } from "./trpc";
-import { inferAsyncReturnType } from "@trpc/server";
-import bodyParser from "body-parser";
-import { IncomingMessage } from "http";
 import nextBuild from "next/dist/build";
 import path from "path";
+import { inferAsyncReturnType } from "@trpc/server";
 
 const app = express();
 const PORT = Number(process.env.PORT) || 3000;
@@ -22,14 +20,7 @@ const createContext = ({
 
 export type ExpressContext = inferAsyncReturnType<typeof createContext>;
 
-export type WebhookRequest = IncomingMessage & { rawBody: Buffer };
-
 const start = async () => {
-  const webhookMiddleware = bodyParser.json({
-    verify: (req: WebhookRequest, _, buffer) => {
-      req.rawBody = buffer;
-    },
-  });
 
   const payload = await getPayloadClient({
     initOptions: {
